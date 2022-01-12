0c248dc0d9eeac471dc3d5b6889f364766ff907c
Lolpawer <dadu.petrosino@gmail.com>
2022-01-12 17:12:58 +0100

    milestone 3

--- js/script.js
+++ js/script.js
@@ -113,17 +113,38 @@ let fontList = [
 	}
 ];
 
-const iconsOutput = document.getElementById("icons-container");
+function createIcon (arrayName, outputBox) {
+	arrayName.forEach((element) => {
+		outputBox.innerHTML += `
+			<div class="icon-box ${element.type} filter-all">
+				<div class="row">
+					<div class="${element.family} ${element.prefix}${element.name} ${element.color}">
+					</div>
+					<p>${element.name.toUpperCase()}</p>
+				</div>
+			</div>
+			</br>
+			`
+	});
+}
 
-fontList.forEach((element) => {
-    iconsOutput.innerHTML += `
-        <div class="icon-box">
-            <div class="row">
-                <div class="${element.family} ${element.prefix}${element.name} ${element.color} ${element.type}">
-                </div>
-                <p>${element.name.toUpperCase()}</p>
-            </div>
-        </div>
-        </br>
-        `
+const iconsOutput = document.getElementById("icons-container");
+const filterButton = document.getElementById("filter");
+
+createIcon(fontList, iconsOutput); //per caricare le icone appena carica il sito
+
+filterButton.addEventListener('change', (event) => {
+
+	iconsOutput.innerHTML = "";
+
+	createIcon(fontList, iconsOutput);
+
+	let iconBoxArray = document.querySelectorAll(".icon-box");
+
+	for (let i = 0; i < iconBoxArray.length; i++) {
+		if (iconBoxArray[i].classList.contains(event.target.value) == false) {
+			iconBoxArray[i].classList.add("hide");
+		};
+	};
+	
 });
